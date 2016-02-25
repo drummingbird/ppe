a561af6a7db5ba039dac2701d9da4025cba8329c
drummingbird <notionsincognito@gmail.com>
2016-02-25 15:23:29 +1300

    Created the basic exercise model and view.

--- mqo_exercises/models/exercise.py
+++ mqo_exercises/models/exercise.py
@@ -1,2 +1,8 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api
+
+class Exercise(models.Model):
+    _name = 'mqo.exercise'
+    
+    name = fields.Char(string="Name", required=True)
+    instructions = fields.Text()
