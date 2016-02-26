e2fdb87b56257d04835f8ba26b24ad88ddc1826b
drummingbird <notionsincognito@gmail.com>
2016-02-26 14:18:21 +1300

    Added a comodel for the next_exercise_id field.

--- mqo_exercises/models/partner.py
+++ mqo_exercises/models/partner.py
@@ -7,12 +7,12 @@ class Partner(models.Model):
     allocation_ids = fields.One2many('mqo.allocation', 'partner_id', string="Allocated Exercises")
     assignment_ids = fields.One2many('mqo.assignment', 'partner_id', string="Assigned Exercises")
     
-    next_exercise_id = fields.Many2one(store=True, readonly=True, compute='_compute_next_exercise')
+    next_exercise_id = fields.Many2one(comodel_name='mqo.allocation', store=True, readonly=True, compute='_compute_next_exercise')
     
     @api.depends('allocation_ids')
-    def _compute_next_exercise(self):
+    def _compute_next_allocation(self):
         for r in self:
-            next_exercise_collection = []
+            next_allocation_collection = []
             if r.allocation_ids:
-                next_exercise_collection.append(r.allocation_ids[0])
-            r.next_exercise_id = next_exercise_collection
+                next_allocation_collection.append(r.allocation_ids[0])
+            r.next_exercise_id = next_allocation_collection
