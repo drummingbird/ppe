d8ddfa617810c4fd1f587e5bb6cc5a5bf92246ca
drummingbird <notionsincognito@gmail.com>
2016-02-26 14:37:14 +1300

    Improvements to views/partner.xml and hopefully fixing next exercise.

--- mqo_exercises/models/partner.py
+++ mqo_exercises/models/partner.py
@@ -7,12 +7,12 @@ class Partner(models.Model):
     allocation_ids = fields.One2many('mqo.allocation', 'partner_id', string="Allocated Exercises")
     assignment_ids = fields.One2many('mqo.assignment', 'partner_id', string="Assigned Exercises")
     
-    next_exercise_id = fields.Many2one(comodel_name='mqo.allocation', store=True, readonly=True, compute='_compute_next_allocation')
+    next_exercise_id = fields.Many2one(comodel_name='mqo.exercise', store=True, readonly=True, compute='_compute_next_exercise', string="Next Exercise")
     
     @api.depends('allocation_ids')
-    def _compute_next_allocation(self):
+    def _compute_next_exercise(self):
         for r in self:
-            next_allocation_collection = []
+            next_exercise_collection = []
             if r.allocation_ids:
-                next_allocation_collection.append(r.allocation_ids[0])
-            r.next_exercise_id = next_allocation_collection
+                next_exercise_collection.append(r.allocation_ids[0].exercise_id)
+            r.next_exercise_id = next_exercise_collection
