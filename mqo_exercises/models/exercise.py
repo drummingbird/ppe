e81bb31e9a4a2572e52ece6c6e16696c1232090a
drummingbird <notionsincognito@gmail.com>
2016-02-29 12:39:43 +1300

    Bugfixing.

--- mqo_exercises/models/exercise.py
+++ mqo_exercises/models/exercise.py
@@ -10,13 +10,13 @@ class Exercise(models.Model):
     assignment_ids = fields.One2many('mqo.assignment', 'exercise_id', string="Assigned exercises")
     
     # Exercise params
-    a.sig_m = fields.Float(string="sig_m")
-    a.sig_c = fields.Float(string="sig_c")
-    a.sig_r = fields.Float(string="sig_r")
-    a.sig_e = fields.Float(string="sig_e")
-    a.exp_m = fields.Float(string="exp_m")
-    a.exp_c = fields.Float(string="exp_c")
-    a.exp_r = fields.Float(string="exp_r")
-    a.exp_e = fields.Float(string="exp_e")
+    sig_m = fields.Float(string="sig_m")
+    sig_c = fields.Float(string="sig_c")
+    sig_r = fields.Float(string="sig_r")
+    sig_e = fields.Float(string="sig_e")
+    exp_m = fields.Float(string="exp_m")
+    exp_c = fields.Float(string="exp_c")
+    exp_r = fields.Float(string="exp_r")
+    exp_e = fields.Float(string="exp_e")
     
     
