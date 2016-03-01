f7a4ca1274a461b9ceb3f01634c74d16b4d457dd
drummingbird <notionsincognito@gmail.com>
2016-03-01 16:16:59 +1300

    Added more exercise parameters.

--- mqo_exercises/models/exercise.py
+++ mqo_exercises/models/exercise.py
@@ -1,6 +1,26 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api
 
+class ExTimePeriod(models.Model):
+    _name = 'mqo.exTimePeriod'
+    
+    exercise_id = fields.Many2one('mqo.exercise',
+        ondelete='cascade', string="Exercise", required=True)
+    start_date = fields.Datetime(string="Start date", required=True, default=lambda self: fields.Datetime.now())
+    end_date = fields.Datetime(string="End date", required=True, default=lambda self: fields.Datetime.now())
+    mag = fields.Float(string="mag", default=0.0)
+
+class ExBoosted(models.Model):
+    _name = 'mqo.exBoosted'
+    
+    exercise_id = fields.Many2one('mqo.exercise',
+        ondelete='cascade', string="Exercise", required=True)
+    boost_exercise_id = fields.Many2one('mqo.exercise',
+        ondelete='cascade', string="Exercise", required=True)
+    mag = fields.Float(string="mag", default=0.0)
+    duration = fields.Float(string="duration", default=0.0)
+
+
 class Exercise(models.Model):
     _name = 'mqo.exercise'
     
@@ -18,5 +38,12 @@ class Exercise(models.Model):
     exp_c = fields.Float(string="exp_c", default=0.0)
     exp_r = fields.Float(string="exp_r", default=1.0)
     exp_e = fields.Float(string="exp_e", default=0.0)
-    
-    
+    bur_m = fields.Float(string="bur_m", default=0.0)
+    bur_c = fields.Float(string="bur_c", default=0.0)
+    bur_r = fields.Float(string="bur_r", default=1.0)
+    bur_e = fields.Float(string="bur_e", default=0.0)
+    bur_c2 = fields.Float(string="bur_c2", default=0.0)
+    bst_m = fields.Float(string="bst_m", default=0.0)
+    dur = fields.Float(string="dur", default=1.0)
+    tper = fields.One2many('mqo.exTimePeriod', 'exercise_id', string="Time periods")
+    bstEx = fields.One2many('mqo.exBoosted', 'exercise_id', string="Boosted exercises")
