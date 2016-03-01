0c29aabc3e2d1f7ed6d9bc5059ad8be5e8ac3914
drummingbird <notionsincognito@gmail.com>
2016-03-01 16:43:30 +1300

    Bugfixing.

--- mqo_exercises/models/exercise.py
+++ mqo_exercises/models/exercise.py
@@ -25,7 +25,7 @@ class ExBoosted(models.Model):
 class Exercise(models.Model):
     _name = 'mqo.exercise'
     
-    name = -fields.Char(string="Title", required=True)
+    name = fields.Char(string="Title", required=True)
     instructions = fields.Text(string="Instructions")
     allocation_ids = fields.One2many('mqo.allocation', 'exercise_id', string="Allocated Exercises")
     assignment_ids = fields.One2many('mqo.assignment', 'exercise_id', string="Assigned exercises")
