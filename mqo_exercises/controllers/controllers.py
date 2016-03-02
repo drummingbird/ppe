c60dd5df0cacfba3bb90c350d029c71c2ed0ae37
drummingbird <notionsincognito@gmail.com>
2016-03-02 17:30:08 +1300

    Bugfixing.

--- mqo_exercises/controllers/controllers.py
+++ mqo_exercises/controllers/controllers.py
@@ -6,7 +6,7 @@ class MyLearning(http.Controller):
     @http.route('/mylearning/mylearning/', auth='public', website=True)
     def index(self, **kw):
         # Partners = http.request.env['res.partner']
-        partner = request.env['res.users'].browse(request.uid).partner_id
+        partner = http.request.env['res.users'].browse(request.uid).partner_id
         if partner:
             currentex = partner.next_exercise_id
             return http.request.render('mqo_exercises.index', {
