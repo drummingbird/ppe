6063e0cdb2861bfdfc49ab438c5b6765b009ff50
drummingbird <notionsincognito@gmail.com>
2016-03-02 16:24:12 +1300

    Started adding security access.

--- mqo_exercises/__openerp__.py
+++ mqo_exercises/__openerp__.py
@@ -22,7 +22,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'templates/templates.xml',
         'views/exercises.xml',
         'views/partner.xml',
