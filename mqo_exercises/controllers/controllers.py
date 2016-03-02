84aae419409bcb8f372860216f71918215df6c3c
drummingbird <notionsincognito@gmail.com>
2016-03-02 17:32:25 +1300

    Bugfixing.

--- mqo_exercises/controllers/controllers.py
+++ mqo_exercises/controllers/controllers.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 from openerp import http
-
+from openerp.http import request
 
 class MyLearning(http.Controller):
     @http.route('/mylearning/mylearning/', auth='public', website=True)
     def index(self, **kw):
         # Partners = http.request.env['res.partner']
-        partner = http.request.env['res.users'].browse(request.uid).partner_id
+        partner = request.env['res.users'].browse(request.uid).partner_id
         if partner:
             currentex = partner.next_exercise_id
-            return http.request.render('mqo_exercises.index', {
+            return request.render('mqo_exercises.index', {
                 'exercises': currentex
             })
         else:
-            return http.request.render('mqo_exercises.index', {'exercises': []})
+            return request.render('mqo_exercises.index', {'exercises': []})
