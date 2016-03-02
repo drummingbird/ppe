99d3a310c7534273587adbf13174422b4af267ae
drummingbird <notionsincognito@gmail.com>
2016-03-02 16:57:32 +1300

    Bugfixing.

--- mqo_exercises/controllers/controllers.py
+++ mqo_exercises/controllers/controllers.py
@@ -5,7 +5,7 @@ from openerp import http
 class MyLearning(http.Controller):
     @http.route('/mylearning/mylearning/', auth='public', website=True)
     def index(self, **kw):
-        Partners = http.request.env['res_partner']
+        Partners = http.request.env['res.partner']
         currentuser = Partners.search([('name', '=', 'Tester 4')])
         currentex = currentuser.next_exercise_id
         return http.request.render('mqo_exercises.index', {
