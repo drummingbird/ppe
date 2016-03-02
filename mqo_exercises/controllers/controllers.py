414aa8043348f80be2a78d10d2267c83cbdb7837
drummingbird <notionsincognito@gmail.com>
2016-03-02 17:18:54 +1300

    Getting current partner_id for website exercise display.

--- mqo_exercises/controllers/controllers.py
+++ mqo_exercises/controllers/controllers.py
@@ -5,9 +5,12 @@ from openerp import http
 class MyLearning(http.Controller):
     @http.route('/mylearning/mylearning/', auth='public', website=True)
     def index(self, **kw):
-        Partners = http.request.env['res.partner']
-        currentuser = Partners.search([('name', '=', 'Tester 4')])
-        currentex = currentuser.next_exercise_id
-        return http.request.render('mqo_exercises.index', {
-            'exercises': currentex
-        })
+        # Partners = http.request.env['res.partner']
+        partner = request.env['res.users'].browse(request.uid).partner_id
+        if partner:
+            currentex = partner.next_exercise_id
+            return http.request.render('mqo_exercises.index', {
+                'exercises': currentex
+            })
+        else:
+            return http.request.render('mqo_exercises.index', {'exercises': []})
