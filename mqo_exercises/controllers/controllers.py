b90cce96f273a4475188747e43b3171af8375942
drummingbird <notionsincognito@gmail.com>
2016-03-02 17:37:37 +1300

    Bugfixing.

--- mqo_exercises/controllers/controllers.py
+++ mqo_exercises/controllers/controllers.py
@@ -8,8 +8,10 @@ class MyLearning(http.Controller):
         # Partners = http.request.env['res.partner']
         partner = request.env['res.users'].browse(request.uid).partner_id
         print(str(partner.name))
+        print(str(partner.id))
         if partner:
-            currentex = partner.next_exercise_id
+            ResPartner = request.env['res.partner'].browse(partner.id)
+            currentex = ResPartner.next_exercise_id
             print(str(currentex.name))
             return request.render('mqo_exercises.index', {
                 'exercises': currentex
