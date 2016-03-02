7f1994ce53b0dcd38647c41c4a44ab73b7f90978
drummingbird <notionsincognito@gmail.com>
2016-03-02 16:45:18 +1300

    Bugfixing.

--- mqo_exercises/controllers/controllers.py
+++ mqo_exercises/controllers/controllers.py
@@ -6,6 +6,6 @@ class MyLearning(http.Controller):
     @http.route('/mylearning/mylearning/', auth='public', website=True)
     def index(self, **kw):
         Exercises = http.request.env['mqo.exercise']
-        return http.request.render('mqo_index', {
+        return http.request.render('mqo_exercises.index', {
             'exercises': Exercises.search([])
         })
