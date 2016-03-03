72b818d573504219db3e88e960445c278cee0cbc
drummingbird <notionsincognito@gmail.com>
2016-03-04 12:29:01 +1300

    Starting to use the survey module to add response capability to exercises.

--- mqo_exercises/controllers/controllers.py
+++ mqo_exercises/controllers/controllers.py
@@ -3,7 +3,7 @@ from openerp import http
 from openerp.http import request
 
 class MyLearning(http.Controller):
-    @http.route('/mylearning/mylearning/', auth='public', website=True)
+    @http.route('/mylearning/mylearning/', type='http', auth='public', website=True)
     def index(self, **kw):
         # Partners = http.request.env['res.partner']
         partner = request.env['res.users'].browse(request.uid).partner_id
@@ -15,3 +15,57 @@ class MyLearning(http.Controller):
             })
         else:
             return request.render('mqo_exercises.index', {'exercises': []})
+
+        
+class ExerciseResponse(http.Controller):
+    
+    def _check_bad_cases(self, cr, uid, request, assignment, assignment_obj, token, context=None):
+        # In case of bad survey, redirect to surveys list
+        if assignment_obj.exists(cr, SUPERUSER_ID, assignment.id, context=context) == []:
+            print('assignment not found')
+            return request.website.render("website.403")
+
+        # redirect if token is invalid.
+        if assignment.response_token != token:
+            print('response token was invalid')
+            return request.website.render("website.403")
+
+        # In case of auth required, block public user
+        if uid == request.website.user_id.id:
+            print('authorisation required')
+            return request.website.render("survey.auth_required", {'survey': assignment.response_survey})
+        
+        # no surveys assigned
+        if not assignment.response_survey:
+            print('assignment has no response surveys')
+            return request.website.render("website.403")
+
+        # Everything seems to be ok
+        return None
+    
+# Survey wrapper for starting a response
+    @http.route(['/exercise/response/<model("mqo.assignment"):assignment>/<string:token>'],
+                type='http', auth='public', website=True)
+    def survey_wrapper(self, assignment, token, prev=None, **post):
+        '''wraps a response'''
+        cr, uid, context = request.cr, request.uid, request.context
+        assignment_obj = request.registry['mqo.assignment']
+        survey_obj = request.registry['survey.survey']
+        user_input_obj = request.registry['survey.user_input']
+
+        # Controls if the survey can be displayed
+        errpage = self._check_bad_cases(cr, uid, request, assignment, assignment_obj, token, context=context)
+        if errpage:
+            return errpage
+
+        # see if response for this assignment already exists, and create one for this assignment if needed.
+        try:
+            user_input_id = user_input_obj.search(cr, SUPERUSER_ID, [('token', '=', assignment.response_token)], context=context)[0]
+        except IndexError:  # No response currently exists for this assignment
+            vals = {'survey_id': assignment.survey, 'partner_id': assignment.partner_id, 'token': assignment.response_token, 'state': 'Not started yet'}
+            user_input_id = user_input_obj.create(cr, uid, vals, context=context)
+            user_input = user_input_obj.browse(cr, uid, [user_input_id], context=context)[0]
+        else:
+            user_input = user_input_obj.browse(cr, SUPERUSER_ID, [user_input_id], context=context)[0]
+
+        return request.redirect('/survey/fill/%s/%s' % (survey.id, user_input.token))
--- mqo_exercises/models/assignment.py
+++ mqo_exercises/models/assignment.py
@@ -2,6 +2,9 @@
 from openerp import models, fields, api
 from datetime import datetime, timedelta
 
+import uuid
+
+
 class Assignment(models.Model):
     _name = 'mqo.assignment'
     
@@ -9,4 +12,13 @@ class Assignment(models.Model):
     exercise_id = fields.Many2one('mqo.exercise',
         ondelete='cascade', string="Exercise", required=True)
     rating = fields.Float(string="Rating")
+    difficulty = fields.Float(string="Difficulty")
     datetime_allocated = fields.Datetime(string="Date Time Stamp", required=True, default=lambda self: fields.Datetime.now())
+    
+    response_survey = fields.Many2one('survey.survey', string="Response survey")
+    responses = fields.Many2one('survey.user_input', string="Responses")
+    response_token = field.String(string="Token", default=lambda s, cr, uid, c: uuid.uuid4().__str__())
+    
+    # can add entries (while doing the exercise) as survey responses too.
+
+    # add function to set rating and difficulty based on responses.
