1b198a0b7f5abdcdc6892bc965e87ecf578c9a5c
drummingbird <notionsincognito@gmail.com>
2016-03-04 15:37:01 +1300

    Testing a different way to reroute exercise responses.

--- mqo_exercises/controllers/controllers.py
+++ mqo_exercises/controllers/controllers.py
@@ -77,8 +77,57 @@ class ExerciseResponse(http.Controller):
         return request.redirect('/exercise/fill/%s/%s' % (assignment.response_survey.id, user_input.token))
     
 class responseReroute(WebsiteSurvey):
-        @http.route(['/exercise/fill/<model("survey.survey"):survey>/<string:token>'],
-                type='http', auth='public', website=True)
-        def fill_exercise(self, *args, **kwargs):
-            return super(responseReroute, self).fill_survey(*args, **kwargs)
-        
+    @http.route(['/exercise/fill/<model("survey.survey"):survey>/<string:token>'],
+            type='http', auth='public', website=True)
+    def fill_exercise(self, *args, **kwargs):
+        return super(responseReroute, self).fill_survey(*args, **kwargs)
+    
+    # AJAX submission of a page
+    @http.route(['/survey/submit/<model("survey.survey"):survey>'],
+                type='http', methods=['POST'], auth='public', website=True)
+    def submit(self, survey, **post):
+        _logger.debug('Incoming data: %s', post)
+        page_id = int(post['page_id'])
+        cr, uid, context = request.cr, request.uid, request.context
+        survey_obj = request.registry['survey.survey']
+        questions_obj = request.registry['survey.question']
+        questions_ids = questions_obj.search(cr, uid, [('page_id', '=', page_id)], context=context)
+        questions = questions_obj.browse(cr, uid, questions_ids, context=context)
+
+        # Answer validation
+        errors = {}
+        for question in questions:
+            answer_tag = "%s_%s_%s" % (survey.id, page_id, question.id)
+            errors.update(questions_obj.validate_question(cr, uid, question, post, answer_tag, context=context))
+
+        ret = {}
+        if (len(errors) != 0):
+            # Return errors messages to webpage
+            ret['errors'] = errors
+        else:
+            # Store answers into database
+            user_input_obj = request.registry['survey.user_input']
+
+            user_input_line_obj = request.registry['survey.user_input_line']
+            try:
+                user_input_id = user_input_obj.search(cr, SUPERUSER_ID, [('token', '=', post['token'])], context=context)[0]
+            except KeyError:  # Invalid token
+                return request.website.render("website.403")
+            user_input = user_input_obj.browse(cr, SUPERUSER_ID, user_input_id, context=context)
+            user_id = uid if user_input.type != 'link' else SUPERUSER_ID
+            for question in questions:
+                answer_tag = "%s_%s_%s" % (survey.id, page_id, question.id)
+                user_input_line_obj.save_lines(cr, user_id, user_input_id, question, post, answer_tag, context=context)
+
+            go_back = post['button_submit'] == 'previous'
+            next_page, _, last = survey_obj.next_page(cr, uid, user_input, page_id, go_back=go_back, context=context)
+            vals = {'last_displayed_page_id': page_id}
+            if next_page is None and not go_back:
+                vals.update({'state': 'done'})
+            else:
+                vals.update({'state': 'skip'})
+            user_input_obj.write(cr, user_id, user_input_id, vals, context=context)
+            ret['redirect'] = '/exercise/fill/%s/%s' % (survey.id, post['token'])
+            if go_back:
+                ret['redirect'] += '/prev'
+        return json.dumps(ret)
