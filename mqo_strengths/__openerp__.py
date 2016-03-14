2f06c8acab29cccae9ce968ddb738ecda865dad5
drummingbird <notionsincognito@gmail.com>
2016-03-14 14:59:36 +1300

    Initial commit of mqo_strenths

---
+++ mqo_strengths/__openerp__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "MQO Strengths",
+
+    'summary': """Manage strengths""",
+
+    'description': """
+        MQO module for managing strengths:
+    """,
+
+    'author': "Your Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'survey'],
+
+    # always loaded
+    'data': [
+        #'security/ir.model.access.csv',
+        'templates/templates.xml',
+        'views/survey.xml',
+        #'views/session_workflow.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo.xml',
+    ],
+}
---
+++ mqo_strengths/controllers/controllers.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+from openerp import http
+from openerp.http import request
+from openerp import SUPERUSER_ID
+
+from openerp.addons.survey.controllers.main import WebsiteSurvey
+import json
+import logging
+
+_logger = logging.getLogger(__name__)
+
+        
+class StrengthsResults(http.Controller):
+    
+    def _check_bad_cases(self, cr, uid, request, survey_obj, survey, user_input_obj, context=None):
+        # In case of bad survey, redirect to surveys list
+        if survey_obj.exists(cr, SUPERUSER_ID, survey.id, context=context) == []:
+            return werkzeug.utils.redirect("/survey/")
+
+        # In case of auth required, block public user
+        if survey.auth_required and uid == request.website.user_id.id:
+            return request.website.render("survey.auth_required", {'survey': survey})
+
+        # In case of non open surveys
+        if survey.stage_id.closed:
+            return request.website.render("survey.notopen")
+
+        # If there is no pages
+        if not survey.page_ids:
+            return request.website.render("survey.nopages")
+
+        # Everything seems to be ok
+        return None
+
+# Survey wrapper for strengths survey response
+    @http.route(['/mystrengths/<string:token>'],
+                type='http', auth='public', website=True)
+    def survey_wrapper(self, token, prev=None, **post):
+        '''wraps a response'''
+        cr, uid, context = request.cr, request.uid, request.context
+        survey_obj = request.registry['survey.survey']
+        user_input_obj = request.registry['survey.user_input']
+
+        survey_id = survey_obj.search(cr, SUPERUSER_ID, [('title', '=', 'Strengths')], context=context)[0]
+        survey = survey_obj.browse(cr, SUPERUSER_ID, [survey_id], context=context)[0]
+        
+        # Controls if the survey can be displayed
+        errpage = self._check_bad_cases(cr, uid, request, survey_obj, survey, user_input_obj, context=context)
+        if errpage:
+            return errpage
+
+        # see if response for this survey already exists.
+        try:
+            user_input_id = user_input_obj.search(cr, SUPERUSER_ID, [('token', '=', token), ('survey_id', '=', survey_id)], context=context)[0]
+        except IndexError:  # No response currently exists for this assignment
+            print('response token was invalid')
+            return request.website.render("website.403")
+        else:
+            user_input = user_input_obj.browse(cr, SUPERUSER_ID, [user_input_id], context=context)[0]
+
+        # Define dictionary to interpret results: question_id -- category
+        ir_model_data = self.pool.get('ir.model.data')
+        
+        dic_ids = dict()
+        for page in survey.page_ids:
+            for question in page.question_ids:
+                dat = ir_model_data.name_get(cr, uid, ids, context=context)
+                dic_ids[question.id] = dat[0]
+        
+        
+        # Identify what category each question is in
+        dic_cat = dic_ids
+        # strengths questions are named X_X_X_{strengthname}_X 
+        for item, value in dic_cat.iteritems():
+                dic_cat[item] = value.split('_')[3]
+        
+        # create result dictionary including each unique category
+        categories = set(dic_cat.values())
+        res = dict()
+        for item in categories:
+            res[item] = 0
+        
+        # get responses
+        user_input_line_obj = request.registry['survey.user_input_line']
+        user_input_line_ids = user_input_line_obj.search(cr, SUPERUSER_ID, [('survey_id', '=', survey_id)], context=context)
+        user_input_line = user_input_line_obj.browse(cr, SUPERUSER_ID, [user_input_line_ids], context=context)
+        
+        # analyze survey results, using dictionary of question ids and categories 
+        for line in user_input_line:
+            cat = dic_cat[line.question_id]
+            res[cat] = res[cat] + line.quizz_mark
+        
+        res_sorted = []  
+        # Sort through strengths in order
+        for key, value in sorted(t.iteritems(), key=lambda (k,v): (v,k),reverse=True):  
+            diction= {"value": value, "name": key}  
+            res_sorted.append(diction)
+
+        vals = {'strengths': res_sorted}
+        return request.website.render("mqo_strengths.results", vals)    
+            
+        
