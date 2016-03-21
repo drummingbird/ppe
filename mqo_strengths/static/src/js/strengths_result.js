85ed9c585767d732c83828f7dd95c91ea313bdf8
drummingbird <notionsincognito@gmail.com>
2016-03-21 16:27:39 +1300

    Bugfixing barchart of strengths.

--- mqo_strengths/static/src/js/strengths_result.js
+++ mqo_strengths/static/src/js/strengths_result.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
 			.showValues(true)
 			.tooltips(true)
 			.showControls(false)
-			.yAxis.scale().domain([1, 5]);
+			.forceY([1, 5]);
 
 		chart.yAxis
 			.tickFormat(d3.format(',.2f'));
