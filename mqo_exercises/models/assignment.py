27edfa9496e24ba51625dea3917837187e4cbdd5
drummingbird <notionsincognito@gmail.com>
2016-03-24 10:44:21 +1300

    Added a state field to assignments.

--- mqo_exercises/models/assignment.py
+++ mqo_exercises/models/assignment.py
@@ -11,6 +11,10 @@ class Assignment(models.Model):
     learner = fields.Many2one('mqo.learner', string="Learner", required=True)
     exercise_id = fields.Many2one('mqo.exercise',
         ondelete='cascade', string="Exercise", required=True)
+    state =  fields.Selection([('new', 'Not started yet'),
+                               ('skip', 'Skipped'),
+                               ('done', 'Completed')],
+                               string='Status', readonly=True)
     rating = fields.Float(string="Rating")
     difficulty = fields.Float(string="Difficulty")
     datetime_allocated = fields.Datetime(string="Date Time Stamp", required=True, default=lambda self: fields.Datetime.now())
