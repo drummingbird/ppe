a3d449fe461e7e89ae65f0d4632105c60c1d23e4
drummingbird <notionsincognito@gmail.com>
2016-03-24 16:12:13 +1300

    Bugfixing security.

--- mqo_learner/__openerp__.py
+++ mqo_learner/__openerp__.py
@@ -22,7 +22,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/learner.xml',
         #'views/session_workflow.xml',
     ],
